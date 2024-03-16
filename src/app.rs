fc03b8ecbb2f0bedf2488908b6cbe0308523c383
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-03-16 03:01:46 -0700

    Add App trait and allow providing a custom state

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,36 @@
+use async_trait::async_trait;
+
 use crate::config::app_config::AppConfig;
+use crate::context::{AppContext, FromAppContextRef, GetAppContextFromRef};
 use crate::tracing::init_tracing;
 
-pub async fn start() -> anyhow::Result<()> {
+pub async fn start<A>() -> anyhow::Result<()>
+where
+    A: App + Default + Sync,
+{
     let config = AppConfig::new()?;
-    init_tracing(&config)?;
+
+    let app = A::default();
+    app.init_tracing(&config)?;
+
+    let context = AppContext::new(config).await?;
+    let _context = app.context_to_state(&context).await?;
 
     Ok(())
+}
+
+#[async_trait]
+pub trait App {
+    type State: FromAppContextRef + GetAppContextFromRef;
+
+    fn init_tracing(&self, config: &AppConfig) -> anyhow::Result<()> {
+        init_tracing(config)?;
+
+        Ok(())
+    }
+
+    async fn context_to_state(&self, app_context: &AppContext) -> anyhow::Result<Self::State> {
+        let state = Self::State::from_app_context_ref(app_context).await?;
+        Ok(*state)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@
 
 pub mod app;
 pub mod config;
+pub mod context;
 pub mod tracing;
