aceab2956240766d518dc66d2fd777a9f2908815
Spencer Ferris <110017625+wgoyiczoyj@users.noreply.github.com>
2024-03-16 22:29:59 -0700

    Enable defining routes using Axum or Aide routers

--- src/app_context.rs
+++ src/app_context.rs
@@ -1,13 +1,21 @@
 use crate::config::app_config::AppConfig;
+use aide::openapi::OpenApi;
+use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub struct AppContext {
     pub config: AppConfig,
+    pub api: Option<Arc<OpenApi>>,
 }
 
 impl AppContext {
     pub async fn new(config: AppConfig) -> anyhow::Result<Self> {
-        let context = Self { config };
+        let context = Self { config, api: None };
         Ok(context)
+    }
+
+    pub fn add_api(&mut self, api: Arc<OpenApi>) -> &mut Self {
+        self.api = Some(api);
+        self
     }
 }
---
+++ src/controller/mod.rs
@@ -0,0 +1,26 @@
+use aide::axum::ApiRouter;
+use axum::Router;
+use itertools::Itertools;
+
+mod ping;
+
+pub fn build_path(parent: &str, child: &str) -> String {
+    // Clean the path to make sure it is valid:
+    // 1. Remove any occurrences of double `/`, e.g. `/foo//bar`
+    // 2. Remove any trailing `/`
+    // 3. Make sure the path starts with a `/`
+    let path = format!("{parent}/{child}");
+    let path = path.split('/').filter(|s| !s.is_empty()).join("/");
+    let path = format!("/{path}");
+    path
+}
+
+pub fn default_routes<S>(parent: &str) -> (Router<S>, ApiRouter<S>)
+where
+    S: Clone + Send + Sync + 'static,
+{
+    let router = Router::new().merge(ping::routes(parent).0);
+    let api_router = ApiRouter::new().merge(ping::routes(parent).1);
+
+    (router, api_router)
+}
