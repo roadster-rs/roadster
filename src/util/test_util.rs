dbbcb32127dad1c269b70477c13c2b53613f3707
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-05-21 00:51:02 -0700

    Add documentation for the TestCase utility class

--- src/util/test_util.rs
+++ src/util/test_util.rs
@@ -1,6 +1,8 @@
 use insta::internals::SettingsBindDropGuard;
 use std::thread::current;
 
+/// Set the `insta` snapshot suffix. Useful for using `insta` together with `rstest`.
+///
 /// See: https://insta.rs/docs/patterns/
 #[cfg_attr(coverage_nightly, coverage(off))]
 pub fn set_snapshot_suffix(suffix: &str) -> SettingsBindDropGuard {
@@ -9,6 +11,32 @@ pub fn set_snapshot_suffix(suffix: &str) -> SettingsBindDropGuard {
     settings.bind_to_scope()
 }
 
+/// Metadata for an `rstest` testcase. Useful for using `insta` together with `rstest`.
+/// Automatically gets the description of the test case and sets it as the snaphot suffix.
+///
+/// # Examples
+///
+/// ```rust
+///
+/// #[cfg(test)]
+/// mod tests {
+///     use insta::assert_snapshot;
+///     use rstest::{fixture, rstest};
+///     use roadster::util::test_util::TestCase;
+///
+///     #[fixture]
+///     fn test_case() -> TestCase {
+///         Default::default()
+///     }
+///
+///     #[rstest]
+///     #[case(0)] // _test_case.description == case_1
+///     #[case::foo(0)] // _test_case.description == foo
+///     fn test(_test_case: TestCase, #[case] num: u32) {
+///         assert_snapshot!(num);
+///     }   
+/// }
+/// ```
 pub struct TestCase {
     pub description: String,
     _settings_guard: SettingsBindDropGuard,
