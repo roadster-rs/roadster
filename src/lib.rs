d21befc51701b94638fa6883891dd78adccdb0ee
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-07-22 13:42:50 -0700

    docs: Use the main project README.md as the library's top-level docs (#281)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,4 @@
-//! A "Batteries Included" web framework for rust designed to get you moving fast 🏎️. Inspired by other fully-featured
-//! frameworks such as [Rails](https://rubyonrails.org/), [Loco](https://github.com/loco-rs/loco),
-//! and [Poem](https://github.com/poem-web/poem).
-//!
-//! ## Features
-//!
-//! - Built on Tokio's web stack (axum, tower, hyper, tracing). App behavior can be easily extended by taking advantage of
-//!   all the resources in the tokio ecosystem.
-//! - Provides sensible defaults so you can focus on building your app.
-//! - Most of the built-in behavior can be customized or even disabled via per-environment configuration files.
-//! - Uses `#![forbid(unsafe_code)]` to ensure all code in Roadster is 100% safe rust.
-
+#![doc = include_str!("../README.md")]
 #![forbid(unsafe_code)]
 // Ignore the warning that the `coverage_nightly` cfg is not recognized.
 #![cfg_attr(test, allow(unexpected_cfgs))]
