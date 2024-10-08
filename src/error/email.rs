8b3a186046b54bfb232e13d0b4c2de189ea87a6f
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-10-08 13:12:06 -0700

    feat: Add smtp client to `AppContext`
    Use the smtp connection details from the `AppConfig::email::smtp` config
    to build an smtp client using the `lettre` crate.

---
+++ src/error/email.rs
@@ -0,0 +1,18 @@
+use crate::error::Error;
+
+#[derive(Debug, Error)]
+#[non_exhaustive]
+pub enum EmailError {
+    #[cfg(feature = "email-smtp")]
+    #[error(transparent)]
+    Smtp(#[from] lettre::transport::smtp::Error),
+
+    #[error(transparent)]
+    Other(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+impl From<lettre::transport::smtp::Error> for Error {
+    fn from(value: lettre::transport::smtp::Error) -> Self {
+        Self::Email(EmailError::from(value))
+    }
+}
