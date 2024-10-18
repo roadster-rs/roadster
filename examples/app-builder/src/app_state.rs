70525c9cae45eb160cbb0b0b23aab743cddee37a
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2024-10-17 19:02:30 -0700

    Don't use axum-core in examples

--- examples/app-builder/src/app_state.rs
+++ examples/app-builder/src/app_state.rs
@@ -1,4 +1,4 @@
-use axum_core::extract::FromRef;
+use axum::extract::FromRef;
 use roadster::app::context::AppContext;
 
 #[derive(Clone, FromRef)]
--- examples/leptos-ssr/src/app_state.rs
+++ examples/leptos-ssr/src/app_state.rs
@@ -1,4 +1,4 @@
-use axum_core::extract::FromRef;
+use axum::extract::FromRef;
 use leptos::LeptosOptions;
 use leptos_config::ConfFile;
 use roadster::app::context::AppContext;
