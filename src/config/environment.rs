de93610be9c22eb0c2f66ab5aa7b6f711cc645ba
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-01-22 14:19:09 -0800

    Improve Environment coverage

--- src/config/environment.rs
+++ src/config/environment.rs
@@ -29,7 +29,24 @@ const DEVELOPMENT: &str = "development";
 const TEST: &str = "test";
 const PRODUCTION: &str = "production";
 
+pub(crate) const ENVIRONMENT_ENV_VAR_NAME: &str = "ENVIRONMENT";
+
+const ENV_VAR_WITH_PREFIX: &str =
+    concatcp!(ENV_VAR_PREFIX, ENV_VAR_SEPARATOR, ENVIRONMENT_ENV_VAR_NAME);
+
 impl Environment {
+    // This can run before tracing is initialized, so we need to use `println` in order to
+    // log from this method.
+    #[allow(clippy::disallowed_macros)]
+    pub fn new() -> RoadsterResult<Self> {
+        // Get the stage, and validate it by parsing to the Environment enum
+        let environment = env::var(ENV_VAR_WITH_PREFIX)
+            .map_err(|_| anyhow!("Env var `{ENV_VAR_WITH_PREFIX}` not defined."))?;
+        let environment = Self::from_str_impl(&environment, true);
+        println!("Using environment from `{ENV_VAR_WITH_PREFIX}` env var: {environment:?}");
+        Ok(environment)
+    }
+
     fn value_variants_impl<'a>() -> &'a [Self] {
         ENV_VARIANTS.get_or_init(|| {
             vec![
@@ -41,8 +58,8 @@ impl Environment {
         })
     }
 
-    fn from_str_impl(input: &str, ignore_case: bool) -> Result<Self, String> {
-        let env = Self::value_variants_impl()
+    fn from_str_impl(input: &str, ignore_case: bool) -> Self {
+        Self::value_variants_impl()
             .iter()
             .find(|variant| {
                 let values = variant.to_possible_value_impl();
@@ -56,9 +73,6 @@ impl Environment {
             })
             .cloned()
             .unwrap_or_else(|| Environment::Custom(input.to_string()))
-            .clone();
-
-        Ok(env)
     }
 
     fn to_possible_value_impl(&self) -> Vec<String> {
@@ -80,7 +94,7 @@ impl ValueEnum for Environment {
     }
 
     fn from_str(input: &str, ignore_case: bool) -> Result<Self, String> {
-        Self::from_str_impl(input, ignore_case)
+        Ok(Self::from_str_impl(input, ignore_case))
     }
 
     fn to_possible_value(&self) -> Option<PossibleValue> {
@@ -119,31 +133,7 @@ impl FromStr for Environment {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let env = Self::from_str_impl(s, true)?;
-        Ok(env)
-    }
-}
-
-pub(crate) const ENVIRONMENT_ENV_VAR_NAME: &str = "ENVIRONMENT";
-
-const ENV_VAR_WITH_PREFIX: &str =
-    concatcp!(ENV_VAR_PREFIX, ENV_VAR_SEPARATOR, ENVIRONMENT_ENV_VAR_NAME);
-
-impl Environment {
-    // This runs before tracing is initialized, so we need to use `println` in order to
-    // log from this method.
-    #[allow(clippy::disallowed_macros)]
-    pub fn new() -> RoadsterResult<Self> {
-        // Get the stage, and validate it by parsing to the Environment enum
-        let environment = env::var(ENV_VAR_WITH_PREFIX)
-            .map_err(|_| anyhow!("Env var `{ENV_VAR_WITH_PREFIX}` not defined."))?;
-        let environment = <Environment as FromStr>::from_str(&environment).map_err(|err| {
-            anyhow!(
-                "Unable to parse `{ENV_VAR_WITH_PREFIX}` env var with value `{environment}`: {err}"
-            )
-        })?;
-        println!("Using environment from `{ENV_VAR_WITH_PREFIX}` env var: {environment:?}");
-        Ok(environment)
+        Ok(Self::from_str_impl(s, true))
     }
 }
 
@@ -185,6 +175,15 @@ mod tests {
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn environment_from_str(_case: TestCase, #[case] env: String) {
         let env = <Environment as FromStr>::from_str(&env).unwrap();
+        assert_debug_snapshot!(env);
+    }
+
+    #[rstest]
+    #[case(DEVELOPMENT.to_string())]
+    #[case(DEVELOPMENT.to_uppercase())]
+    #[cfg_attr(coverage_nightly, coverage(off))]
+    fn value_enum_from_str(_case: TestCase, #[case] env: String) {
+        let env = <Environment as ValueEnum>::from_str(&env, false).unwrap();
         assert_debug_snapshot!(env);
     }
 
