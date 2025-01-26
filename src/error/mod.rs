ac831526cf97b373d373a7e24cc7d05d39c81624
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-01-26 12:23:18 -0800

    Add missing `#[cfg_attr(coverage_nightly, coverage(off))]`

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -141,6 +141,7 @@ mod tests {
     use rstest::{fixture, rstest};
 
     #[fixture]
+    #[cfg_attr(coverage_nightly, coverage(off))]
     fn case() -> TestCase {
         Default::default()
     }
