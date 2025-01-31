8a76fce18019297450d2216380119f56cc1895e9
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-01-31 00:19:33 -0800

    docs: Add to `AppContext` book docs

---
+++ book/examples/app-context/src/app.rs
@@ -0,0 +1,57 @@
+use crate::state::CustomState;
+use anyhow::anyhow;
+use async_trait::async_trait;
+use axum::extract::FromRef;
+use roadster::app::context::{AppContext, AppContextWeak};
+use roadster::app::RoadsterApp;
+use roadster::error::RoadsterResult;
+use roadster::health::check::{CheckResponse, HealthCheck, Status};
+use roadster::util::empty::Empty;
+use std::time::Duration;
+
+pub type App = RoadsterApp<CustomState, Empty, Empty>;
+
+pub struct ExampleHealthCheck {
+    // Prevent reference cycle because the `ExampleHealthCheck` is also stored in the `AppContext`
+    context: AppContextWeak,
+}
+
+#[async_trait]
+impl HealthCheck for ExampleHealthCheck {
+    fn name(&self) -> String {
+        "example".to_string()
+    }
+
+    fn enabled(&self) -> bool {
+        true
+    }
+
+    async fn check(&self) -> RoadsterResult<CheckResponse> {
+        // Upgrade the `AppContext` in order to use it
+        let _context = self
+            .context
+            .upgrade()
+            .ok_or_else(|| anyhow!("Could not upgrade AppContextWeak"))?;
+
+        Ok(CheckResponse::builder()
+            .status(Status::Ok)
+            .latency(Duration::from_secs(0))
+            .build())
+    }
+}
+
+pub fn build_app() -> App {
+    RoadsterApp::builder()
+        .state_provider(|context| {
+            Ok(CustomState {
+                context,
+                custom_field: "Custom Field".to_string(),
+            })
+        })
+        .add_health_check_provider(|registry, state| {
+            // Downgrade the context before providing it to the `ExampleHealthCheck`
+            let context = AppContext::from_ref(state).downgrade();
+            registry.register(ExampleHealthCheck { context })
+        })
+        .build()
+}
