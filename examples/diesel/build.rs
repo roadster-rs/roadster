4703ef246926ab1ecdd41fdd7009b0edd895af5b
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-02-15 15:18:50 -0800

    --wip-- [skip ci]

--- examples/diesel/build.rs
+++ examples/diesel/build.rs
@@ -1,9 +1,14 @@
 use vergen::Emitter;
 use vergen_gitcl::GitclBuilder;
 
+#[allow(clippy::disallowed_macros)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let gitcl = GitclBuilder::default().sha(true).build()?;
     Emitter::default().add_instructions(&gitcl)?.emit()?;
+
+    // https://docs.rs/diesel_migrations/2.2.0/diesel_migrations/macro.embed_migrations.html#automatic-rebuilds
+    // todo: check that this is correct
+    println!("cargo:rerun-if-changed=migrations");
 
     Ok(())
 }
