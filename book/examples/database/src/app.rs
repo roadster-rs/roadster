d15470e90d58a312c76c01b5c04bfacf9f7c636f
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-02-18 18:32:30 -0800

    docs: Update db chapter of book

--- book/examples/database/src/app.rs
+++ book/examples/database/src/app.rs
@@ -1,6 +1,8 @@
+use crate::migrator::Migrator;
 use roadster::app::context::AppContext;
 use roadster::app::App;
 use roadster::error::RoadsterResult;
+use roadster::migration::sea_orm::SeaOrmMigrator;
 use sea_orm::prelude::async_trait::async_trait;
 
 pub struct MyApp;
@@ -11,5 +13,12 @@ impl App<AppContext> for MyApp {
 
     async fn provide_state(&self, context: AppContext) -> RoadsterResult<AppContext> {
         Ok(context)
+    }
+
+    fn migrators(
+        &self,
+        _state: &AppContext,
+    ) -> RoadsterResult<Vec<Box<dyn roadster::migration::Migrator<AppContext>>>> {
+        Ok(vec![Box::new(SeaOrmMigrator::new(Migrator))])
     }
 }
