ca62a66679ec848b6659979da05aa135fef6bc7b
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-02-23 14:49:49 -0800

    chore: Fix powerset checks

--- src/util/db/mod.rs
+++ src/util/db/mod.rs
@@ -1,4 +1,4 @@
-#[cfg(feature = "db-diesel")]
+#[cfg(any(feature = "db-diesel-postgres", feature = "db-diesel-mysql"))]
 pub(crate) mod diesel;
 #[cfg(feature = "db-sea-orm")]
 pub(crate) mod sea_orm;
