1b6f7ca07d6dd1b3029afa8800a1ac25614ea35e
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-02-25 23:43:48 -0800

    Fix diesel example compile errors

--- book/examples/database-diesel/src/migrator.rs
+++
@@ -1,9 +0,0 @@
-use sea_orm_migration::{MigrationTrait, MigratorTrait};
-
-pub struct Migrator;
-
-impl MigratorTrait for Migrator {
-    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        todo!()
-    }
-}
