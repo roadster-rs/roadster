86fff640a051e7996d56346d17a658c8997c665d
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-02-25 23:28:14 -0800

    docs: Improve diesel docs in book

---
+++ book/examples/database-diesel/src/migrator.rs
@@ -0,0 +1,9 @@
+use sea_orm_migration::{MigrationTrait, MigratorTrait};
+
+pub struct Migrator;
+
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        todo!()
+    }
+}
