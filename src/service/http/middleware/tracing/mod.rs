381b673baba6ec0526a7a1c71b656f14c6e8e233
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-03-27 01:43:20 -0700

    feat!: Follow OTEL conventions for http spans and events
    Update provided HTTP span and events to follow the OTEL semantic
    conventions more closely. This PR includes some breaking changes to the
    constructors of some of our custom trace event handlers for the tower
    trace layer.

--- src/service/http/middleware/tracing/mod.rs
+++ src/service/http/middleware/tracing/mod.rs
@@ -5,20 +5,37 @@ use crate::error::RoadsterResult;
 use crate::service::http::middleware::Middleware;
 use axum::Router;
 use axum::extract::{FromRef, MatchedPath};
-use axum::http::{Request, Response};
+use axum::http::{HeaderMap, HeaderName, HeaderValue, Request, Response};
 use opentelemetry_semantic_conventions::trace::{
-    HTTP_REQUEST_METHOD, HTTP_RESPONSE_STATUS_CODE, HTTP_ROUTE, URL_PATH,
+    HTTP_REQUEST_METHOD, HTTP_RESPONSE_STATUS_CODE, HTTP_ROUTE, NETWORK_PROTOCOL_VERSION,
 };
 use serde_derive::{Deserialize, Serialize};
+use std::collections::{BTreeMap, HashSet};
+use std::str::FromStr;
 use std::time::Duration;
-use tower_http::trace::{DefaultOnResponse, MakeSpan, OnRequest, OnResponse, TraceLayer};
-use tracing::{Level, Span, Value, event, field, info_span};
+use tower_http::trace::{MakeSpan, OnRequest, OnResponse, TraceLayer};
+use tracing::{Span, Value, field, info, info_span};
 use validator::Validate;
 
 #[derive(Debug, Clone, Default, Validate, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case", default)]
 #[non_exhaustive]
-pub struct TracingConfig {}
+pub struct TracingConfig {
+    /// Allow all HTTP request headers to be added as trace attributes. Useful for development and
+    /// test environments. Not recommended to be enabled in production.
+    #[serde(default)]
+    pub request_headers_allow_all: bool,
+    /// Allow all HTTP response headers to be added as trace attributes. Useful for development and
+    /// test environments. Not recommended to be enabled in production.
+    #[serde(default)]
+    pub response_headers_allow_all: bool,
+    /// Allow-list of HTTP request headers to add as trace attributes.
+    #[serde(default)]
+    pub request_header_names: Vec<String>,
+    /// Allow-list of HTTP response headers to add as trace attributes.
+    #[serde(default)]
+    pub response_header_names: Vec<String>,
+}
 
 pub struct TracingMiddleware;
 impl<S> Middleware<S> for TracingMiddleware
@@ -56,22 +73,15 @@ where
 
     fn install(&self, router: Router, state: &S) -> RoadsterResult<Router> {
         let context = AppContext::from_ref(state);
-        let request_id_header_name = &context
-            .config()
-            .service
-            .http
-            .custom
-            .middleware
-            .set_request_id
-            .custom
-            .common
-            .header_name;
+        let middleware_config = &context.config().service.http.custom.middleware;
+        let request_id_header_name = &middleware_config.set_request_id.custom.common.header_name;
+        let tracing_config = &middleware_config.tracing.custom;
 
         let router = router.layer(
             TraceLayer::new_for_http()
-                .make_span_with(CustomMakeSpan::new(request_id_header_name.clone()))
-                .on_request(CustomOnRequest)
-                .on_response(CustomOnResponse::new()),
+                .make_span_with(CustomMakeSpan::new(request_id_header_name))
+                .on_request(CustomOnRequest::new(tracing_config))
+                .on_response(CustomOnResponse::new(tracing_config)),
         );
 
         Ok(router)
@@ -85,29 +95,35 @@ pub struct CustomMakeSpan {
 }
 
 impl CustomMakeSpan {
-    pub fn new(request_id_header_name: String) -> Self {
+    pub fn new(request_id_header_name: &str) -> Self {
         Self {
-            request_id_header_name,
+            request_id_header_name: request_id_header_name.to_owned(),
         }
     }
 }
 
 impl<B> MakeSpan<B> for CustomMakeSpan {
     fn make_span(&mut self, request: &Request<B>) -> Span {
-        let path = get_path(request);
+        let matched_path = get_matched_path(request);
         let request_id = get_request_id(&self.request_id_header_name, request);
-        info_span!("http_request",
+
+        /*
+        The OTEL semantic conventions allow the span name to be `{method} {target}`,
+        e.g., `GET /some/http/route`. However, the tracing crate we use requires the span name
+        to be static. So, we use `HTTP` instead, which is the fallback value specified by OTEL.
+         */
+        info_span!("HTTP",
             { HTTP_REQUEST_METHOD } = %request.method(),
-            { HTTP_ROUTE } = optional_trace_field(path),
+            { HTTP_ROUTE } = optional_trace_field(matched_path),
+            { NETWORK_PROTOCOL_VERSION } = ?request.version(),
             request_id = optional_trace_field(request_id),
-            // Fields that aren't know at request time, but will (may?) be known by
-            // response time
+            // Fields that aren't know at request time, but may be known at response time
             { HTTP_RESPONSE_STATUS_CODE } = field::Empty,
         )
     }
 }
 
-fn get_path<B>(request: &Request<B>) -> Option<&str> {
+fn get_matched_path<B>(request: &Request<B>) -> Option<&str> {
     request
         .extensions()
         .get::<MatchedPath>()
@@ -133,48 +149,116 @@ where
         .unwrap_or(Box::new(field::Empty))
 }
 
-#[derive(Debug, Copy, Clone)]
-pub struct CustomOnRequest;
+#[derive(Debug, Clone)]
+pub struct CustomOnRequest {
+    /// Allow all HTTP request headers to be added as trace attributes. Useful for development and
+    /// test environments. Not recommended to be enabled in production.
+    pub allow_all_headers: bool,
+    /// Allow-list of HTTP request headers to add as trace attributes.
+    pub request_header_names: HashSet<HeaderName>,
+}
+
+impl CustomOnRequest {
+    pub fn new(tracing_config: &TracingConfig) -> Self {
+        let request_header_names = tracing_config
+            .request_header_names
+            .iter()
+            .filter_map(|name| HeaderName::from_str(name).ok())
+            .collect();
+        Self {
+            allow_all_headers: tracing_config.request_headers_allow_all,
+            request_header_names,
+        }
+    }
+}
 
 impl<B> OnRequest<B> for CustomOnRequest {
     fn on_request(&mut self, request: &Request<B>, _: &Span) {
-        event!(
-            Level::INFO,
-            version = ?request.version(),
-            { URL_PATH } = %request.uri(),
-            request_headers = ?request.headers(),
-            "started processing request",
-        )
+        let request_headers = allowed_headers(
+            request.headers(),
+            &self.request_header_names,
+            self.allow_all_headers,
+        );
+
+        /*
+        The OTEL semantic conventions allow for providing request headers via the
+        `http.request.header.<key>` span key. However, this is difficult to support with
+        the tracing crate we're using because it requires specifying all span keys up front with
+        static keys. This means we would need to manually list out all of the possible header
+        names vs dynamically adding span keys. Instead, we include the headers as an attribute
+        on the "request started" event.
+
+        See: <https://docs.rs/tracing/latest/tracing/#recording-fields>
+        See: <https://opentelemetry.io/docs/specs/semconv/attributes-registry/http/>
+         */
+        info!(?request_headers, "started processing request");
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct CustomOnResponse {
-    default: DefaultOnResponse,
+    /// Allow all HTTP response headers to be added as trace attributes. Useful for development and
+    /// test environments. Not recommended to be enabled in production.
+    pub allow_all_headers: bool,
+    /// Allow-list of HTTP response headers to add as trace attributes.
+    pub response_header_names: HashSet<HeaderName>,
 }
 
 impl CustomOnResponse {
-    pub fn new() -> CustomOnResponse {
+    pub fn new(tracing_config: &TracingConfig) -> CustomOnResponse {
+        let response_header_names = tracing_config
+            .response_header_names
+            .iter()
+            .filter_map(|name| HeaderName::from_str(name).ok())
+            .collect();
         CustomOnResponse {
-            default: DefaultOnResponse::new()
-                .include_headers(true)
-                // TODO: Configure the level via AppConfig?
-                .level(Level::INFO),
+            allow_all_headers: tracing_config.response_headers_allow_all,
+            response_header_names,
         }
-    }
-}
-
-impl Default for CustomOnResponse {
-    fn default() -> Self {
-        Self::new()
     }
 }
 
 impl<B> OnResponse<B> for CustomOnResponse {
     fn on_response(self, response: &Response<B>, latency: Duration, span: &Span) {
         span.record(HTTP_RESPONSE_STATUS_CODE, response.status().as_u16());
-        self.default.on_response(response, latency, span);
+
+        let response_headers = allowed_headers(
+            response.headers(),
+            &self.response_header_names,
+            self.allow_all_headers,
+        );
+
+        /*
+        The OTEL semantic conventions allow for providing request headers via the
+        `http.request.header.<key>` span key. However, this is difficult to support with
+        the tracing crate we're using because it requires specifying all span keys up front with
+        static keys. This means we would need to manually list out all of the possible header
+        names vs dynamically adding span keys. Instead, we include the headers as an attribute
+        on the "request started" event.
+
+        See: <https://docs.rs/tracing/latest/tracing/#recording-fields>
+        See: <https://opentelemetry.io/docs/specs/semconv/attributes-registry/http/>
+         */
+        info!(
+            ?response_headers,
+            // The latency can easily be derived from the trace itself. However, the `DefaultOnResponse`
+            // impl includes the latency, so we include it as well.
+            latency = latency.as_millis(),
+            "finished processing request"
+        )
     }
+}
+
+fn allowed_headers<'a>(
+    headers: &'a HeaderMap,
+    allow_list_headers: &'a HashSet<HeaderName>,
+    allow_all: bool,
+) -> BTreeMap<&'a str, &'a HeaderValue> {
+    headers
+        .iter()
+        .filter(|(key, _)| allow_all || allow_list_headers.contains(*key))
+        .map(|(key, value)| (key.as_str(), value))
+        .collect()
 }
 
 #[cfg(test)]
