bec83a0475ee9160e2b206ffe279053de5cb04e8
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-05-18 23:52:59 -0700

    --wip-- [skip ci]

--- src/service/worker/mod.rs
+++ src/service/worker/mod.rs
@@ -1,4 +1,15 @@
-#[cfg_attr(feature = "pg-queue")]
+use crate::error::RoadsterResult;
+use async_trait::async_trait;
+
+#[cfg(feature = "pg-queue")]
 pub mod pg;
 #[cfg(feature = "sidekiq")]
 pub mod sidekiq;
+
+#[async_trait]
+pub trait Worker<Args> {
+    // todo: Make this roadster specific and pass the app-state as a method param? That would
+    //  certainly make it a bit easier to use, which would be nice.
+    // todo: Make general enough to work as a shared/wrapper trait of sidekiq's worker trait?
+    async fn handle(args: Args) -> RoadsterResult<()>;
+}
--- src/service/worker/pg/mod.rs
+++ src/service/worker/pg/mod.rs
@@ -1,1 +1,6 @@
 //! Background task queue service backed by Postgres using [pgmq](https://docs.rs/pgmq).
+
+/*
+- job/task/message
+- runner/worker/handler/processor
+ */
