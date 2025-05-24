f7e38c5bbeb1e6435437e45c33f3605b33a3f38f
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-05-24 01:51:43 -0700

    --wip-- [skip ci]

--- src/service/worker/mod.rs
+++ src/service/worker/mod.rs
@@ -1,15 +1,7 @@
 use crate::error::RoadsterResult;
 use async_trait::async_trait;
 
-#[cfg(feature = "pg-queue")]
+#[cfg(feature = "worker-pg")]
 pub mod pg;
-#[cfg(feature = "sidekiq")]
+#[cfg(feature = "worker-sidekiq")]
 pub mod sidekiq;
-
-#[async_trait]
-pub trait Worker<Args> {
-    // todo: Make this roadster specific and pass the app-state as a method param? That would
-    //  certainly make it a bit easier to use, which would be nice.
-    // todo: Make general enough to work as a shared/wrapper trait of sidekiq's worker trait?
-    async fn handle(args: Args) -> RoadsterResult<()>;
-}
--- src/service/worker/pg/mod.rs
+++ src/service/worker/pg/mod.rs
@@ -4,3 +4,13 @@
 - job/task/message
 - runner/worker/handler/processor
  */
+use crate::error::RoadsterResult;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Worker<Args> {
+    // todo: Make this roadster specific and pass the app-state as a method param? That would
+    //  certainly make it a bit easier to use, which would be nice.
+    // todo: Make general enough to work as a shared/wrapper trait of sidekiq's worker trait?
+    async fn handle(args: Args) -> RoadsterResult<()>;
+}
