3253e2da3cbb6760fd6bc32ac56b636b1ec28d4a
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-05-25 01:08:54 -0700

    --wip-- [skip ci]

--- src/service/worker/pg/mod.rs
+++ src/service/worker/pg/mod.rs
@@ -14,3 +14,54 @@ pub trait Worker<Args> {
     // todo: Make general enough to work as a shared/wrapper trait of sidekiq's worker trait?
     async fn handle(args: Args) -> RoadsterResult<()>;
 }
+
+/*
+Comparison of PGMQueue vs PGMQueueExt methods.
+
+I think we'll want to start with just supporting PGMQueue for now without the extension. PGMQueue
+can do everything PGMQueueExt can do except create partitioned tables, and PGMQueueExt doesn't
+support sending batches of messages.
+
+PGMQueue
+    archive
+    archive_batch
+    create
+    create_unlogged
+    delete
+    delete_batch
+*   destroy -- I think this is the same as `PGMQueueExt#drop_queue`
+    new
+    new_with_pool
+    pop
+    purge -- Deletes all messages in the queue(?). I think this is the same as `PGMQueueExt#purge_queue`
+    read
+*   read_batch
+    read_batch_with_poll
+    send
+*   send_batch
+*   send_batch_delay
+    send_delay
+    set_vt
+
+PGMQueueExt
+    archive
+    archive_batch
+    create
+*   create_partitioned
+    create_unlogged
+    delete
+    delete_batch
+*   drop_queue -- I think this is the same as `PGMQueue#destroy`
+*   init -- Inits the pgmq pg extension
+*   list_queues
+    new
+    new_with_pool
+    pop
+    purge_queue -- rustdoc is the same as `drop_queue`, but I'm guessing this is the same as `PGMQueueExt#purge_queue`
+    read
+    read_batch_with_poll
+    send
+    send_delay
+    set_vt
+
+ */
