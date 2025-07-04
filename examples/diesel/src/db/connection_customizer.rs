bff384460d1bc4e72b63ba9bc179610b62ae1b28
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-07-04 15:31:38 -0700

    Update `taiki-e/install-action`

--- examples/diesel/src/db/connection_customizer.rs
+++ examples/diesel/src/db/connection_customizer.rs
@@ -1,21 +1,25 @@
-use async_trait::async_trait;
 use diesel_async::pooled_connection::{PoolError, PoolableConnection, RecyclingMethod};
 use roadster::db::DieselPgConnAsync;
+use std::pin::Pin;
 use tracing::info;
 
 #[derive(Debug)]
 pub struct CustomConnectionCustomizer;
 
-#[async_trait]
 impl bb8::CustomizeConnection<DieselPgConnAsync, PoolError> for CustomConnectionCustomizer {
-    async fn on_acquire(&self, connection: &mut DieselPgConnAsync) -> Result<(), PoolError> {
-        // Note: this `ping` is redundant with the `database.test-on-checkout` config field
-        let result = connection.ping(&RecyclingMethod::Fast).await;
-        let healthy = result.is_ok();
-        info!(%healthy, "Connection acquired");
-        match result {
-            Ok(_) => Ok(()),
-            Err(err) => Err(PoolError::QueryError(err)),
-        }
+    fn on_acquire<'a>(
+        &'a self,
+        connection: &'a mut DieselPgConnAsync,
+    ) -> Pin<Box<dyn Future<Output = Result<(), PoolError>> + Send + 'a>> {
+        Box::pin(async {
+            // Note: this `ping` is redundant with the `database.test-on-checkout` config field
+            let result = connection.ping(&RecyclingMethod::Fast).await;
+            let healthy = result.is_ok();
+            info!(%healthy, "Connection acquired");
+            match result {
+                Ok(_) => Ok(()),
+                Err(err) => Err(PoolError::QueryError(err)),
+            }
+        })
     }
 }
