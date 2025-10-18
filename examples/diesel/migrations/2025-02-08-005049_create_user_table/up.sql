96aaaa2b9ff5905dbfd875383521d089b4d06423
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-10-18 13:28:10 -0700

    docs: Use pg native uuidv7 for id field default in diesel example (#918)

--- examples/diesel/migrations/2025-02-08-005049_create_user_table/up.sql
+++ examples/diesel/migrations/2025-02-08-005049_create_user_table/up.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS "user" (
-    "created_at" timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "id" uuid NOT NULL PRIMARY KEY,
-    "name" varchar NOT NULL CHECK (CHAR_LENGTH("name") > 0),
-    "username" varchar NOT NULL UNIQUE CHECK (CHAR_LENGTH("username") > 0),
-    "email" varchar NOT NULL UNIQUE CHECK (CHAR_LENGTH("email") > 0),
-    "password" varchar NOT NULL
+CREATE TABLE IF NOT EXISTS "user"
+(
+    "created_at" timestamp with time zone NOT NULL             DEFAULT CURRENT_TIMESTAMP,
+    "id"         uuid                     NOT NULL PRIMARY KEY DEFAULT uuidv7(), -- uuidv7() requires Postgres 18
+    "name"       varchar                  NOT NULL CHECK (CHAR_LENGTH("name") > 0),
+    "username"   varchar                  NOT NULL UNIQUE CHECK (CHAR_LENGTH("username") > 0),
+    "email"      varchar                  NOT NULL UNIQUE CHECK (CHAR_LENGTH("email") > 0),
+    "password"   varchar                  NOT NULL
 )
