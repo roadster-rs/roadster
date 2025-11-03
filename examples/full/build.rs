d0444aa1f8976022a5d737caa98d7fab2c7e0d7c
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2025-11-02 19:57:03 -0800

    chore: Update dependencies (#919)
    Update dependencies with semver breaking version bumps. This will result
    in our own semver version bump.

--- examples/full/build.rs
+++ examples/full/build.rs
@@ -12,7 +12,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(feature = "grpc")]
     {
         let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-        tonic_build::configure()
+        tonic_prost_build::configure()
             .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
             .compile_protos(&["proto/helloworld.proto"], &["proto"])
             .unwrap();
