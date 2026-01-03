b632b3c464f5dffad68a7896657b8ce31632076d
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2026-01-02 23:49:47 -0800

    chore!: Remove deprecated `anyhow` usage from error enum
    Closes https://github.com/roadster-rs/roadster/issues/948

--- src/error/other.rs
+++ src/error/other.rs
@@ -3,24 +3,11 @@ use crate::error::Error;
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum OtherError {
-    #[error(transparent)]
-    #[deprecated(
-        since = "0.7.3",
-        note = "`anyhow` is no longer used internally. This enum variant will be removed in the next semver breaking release."
-    )]
-    Anyhow(#[from] anyhow::Error),
-
     #[error("{0}")]
     Message(String),
 
     #[error(transparent)]
     Other(#[from] Box<dyn Send + Sync + std::error::Error>),
-}
-
-impl From<anyhow::Error> for Error {
-    fn from(value: anyhow::Error) -> Self {
-        Self::Other(OtherError::from(value))
-    }
 }
 
 impl From<Box<dyn Send + Sync + std::error::Error>> for Error {
