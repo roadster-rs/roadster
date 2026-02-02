0b3328078f9bbf690a43b6d9808a1a85885f942d
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2026-02-02 15:28:27 -0800

    fix: Adjust res compression to enable logging before compression (2) (#956)
    Second attempt to adjust the priority of the middleware to allow logging
    responses before they are compressed. Instead of the previous PR (#955)
    which changed the `service.http.middleware.response-compression`
    middleware priority, this PR reverts the previous change and instead
    updates the `service.http.middleware.request-response-logging`
    middleware priority. The reason is that when responses go through the
    middleware, they go in reverse order of when handling a request, so the
    `request-response-logging` middleware needs to be installed after the
    `response-compression` middleware so they run in the correct order when
    handling the response

--- src/service/http/middleware/tracing/req_res_logging.rs
+++ src/service/http/middleware/tracing/req_res_logging.rs
@@ -303,7 +303,7 @@ mod tests {
     }
 
     #[rstest]
-    #[case(None, 0)]
+    #[case(None, 9900)]
     #[case(Some(1234), 1234)]
     #[cfg_attr(coverage_nightly, coverage(off))]
     fn priority(#[case] override_priority: Option<i32>, #[case] expected_priority: i32) {
